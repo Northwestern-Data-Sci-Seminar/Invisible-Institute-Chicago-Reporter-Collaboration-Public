1632c2155f8717d305990dac76d1068bfccc68d5
Muhan Li <hanhanmumuqq@163.com>
2020-10-06 00:29:36 +0800

    CHK1-v2

--- The Defiant Enigmas/checkpoint-1/src/Q3.sql
+++ The Defiant Enigmas/checkpoint-1/src/Q3.sql
@@ -2,7 +2,34 @@ select year, count(officer_id)
 from (select officer_id, extract(year from start_date) as year
       from data_officerallegation
       group by officer_id, extract(year from start_date)
+      having count(officer_id) >= /*allegation threshold*/ 2) as q
+group by year
+order by count(officer_id) desc
+limit 2;
+
+select year, count(officer_id)
+from (select officer_id, extract(year from start_date) as year
+      from data_officerallegation
+      group by officer_id, extract(year from start_date)
+      having count(officer_id) >= /*allegation threshold*/ 3) as q
+group by year
+order by count(officer_id) desc
+limit 2;
+
+select year, count(officer_id)
+from (select officer_id, extract(year from start_date) as year
+      from data_officerallegation
+      group by officer_id, extract(year from start_date)
+      having count(officer_id) >= /*allegation threshold*/ 4) as q
+group by year
+order by count(officer_id) desc
+limit 2;
+
+select year, count(officer_id)
+from (select officer_id, extract(year from start_date) as year
+      from data_officerallegation
+      group by officer_id, extract(year from start_date)
       having count(officer_id) >= /*allegation threshold*/ 5) as q
 group by year
 order by count(officer_id) desc
-limit 1;
+limit 2;
