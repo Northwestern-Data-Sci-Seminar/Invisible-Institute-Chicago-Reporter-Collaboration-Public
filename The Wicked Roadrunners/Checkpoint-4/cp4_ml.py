9d97599330c7286cf80079333c71097716a02a78
Kylie Chesner <kyliec362@gmail.com>
2020-11-11 13:43:01 -0600

    Update cp4_ml.py

--- The Wicked Roadrunners/Checkpoint-4/cp4_ml.py
+++ The Wicked Roadrunners/Checkpoint-4/cp4_ml.py
@@ -5,20 +5,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.svm import SVC
 import numpy as np
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
 from sklearn import preprocessing
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import export_graphviz
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import RandomForestClassifier
+
 
 
 # cp4 question 1: predict discipline
@@ -52,7 +44,7 @@ plt.ylabel('Frequency')
 plt.show()
 
 y = data_over[['disciplined']]
-X = data_over[['complaint_percentile', 'allegation_count']]
+X = data_over[['race', 'rank', 'complaint_percentile', 'allegation_count', 'category']]
 
 # preprocess data to encode categorical data as numeric
 le = preprocessing.LabelEncoder()
@@ -69,59 +61,60 @@ scores = cross_val_score(clf, X, y, cv=5, scoring='accuracy')  # try using 'accu
 print("CV scores = ", scores)
 print(metrics.classification_report(y_test, y_pred, digits=3))
 
-# cp4 question 2: predict settlement amount
-file_path = 'cp4_question_2_predict_settlement_amount.csv'
-df = pd.read_csv (file_path)
-df = pd.DataFrame(df,columns=['gender', 'race', 'rank', 'interactions', 'outcomes', 'misconducts', 'violences', 'primary_cause', 'total_settlement'])
-df = df.dropna()
-df = df[df['total_settlement'] >= 10]
-y = df[['total_settlement']].astype(int)
-y = y.transform(lambda x: np.floor(np.log10(x))) # predict the magnitude (e.g. 10 is 1, 100 is 2, 1000 is 3...)
-X = df[['interactions', 'outcomes', 'misconducts', 'gender', 'race']]
 
-
-# ************* Average settlemt cost by gender *************
-df2 = df[['gender', 'total_settlement']]
-df2 = df2[df2['gender'] == 'F']
-df3 = df[['gender', 'total_settlement']]
-df3 = df3[df3['gender'] == 'M']
-plt.bar(['Female','Male'], [np.int(df2.mean()), np.int(df3.mean())], color='red')
-plt.title("Average settlement cost by gender")
-plt.show()
-
-# ************* Average settlemt cost by race *************
-
-df4 = df[['race', 'total_settlement']]
-df4 = df4[df4['race'] == 'White']
-
-df5 = df[['race', 'total_settlement']]
-df5 = df5[df5['race'] == 'Hispanic']
-
-df6 = df[['race', 'total_settlement']]
-df6 = df6[df6['race'] == 'Black']
-
-df7 = df[['race', 'total_settlement']]
-df7 = df7[df7['race'] == 'Asian/Pacific']
-
-df8 = df[['race', 'total_settlement']]
-df8 = df8[df8['race'] == 'Native American/Alaskan Native']
-
-plt.bar(['White','Hispanic', 'Black', 'Asian/Pacific', 'Native American/Alaskan Native'], [np.int(df4.mean()), np.int(df5.mean()), np.int(df6.mean()), np.int(df7.mean()), np.int(df8.mean())], color='red')
-plt.title("Average settlement cost by race")
-plt.show()
-
-
-
-#preprocess data to encode categorical data as numeric
-le = preprocessing.LabelEncoder()
-for column_name in X.columns:
-    if X[column_name].dtype == object:
-        X[column_name] = le.fit_transform(X[column_name])
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30, random_state = 5)
-clf = KNeighborsClassifier(n_neighbors=3, weights='distance')
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-scores = cross_val_score(clf, X, y, cv=4, scoring='accuracy') #try using 'accuracy', 'precision', 'recall', and 'f1_macro' for the scoring parameter
-print("CV scores = ",scores)
-print(metrics.classification_report(y_test, y_pred, digits=3))
+# # cp4 question 2: predict settlement amount
+# file_path = 'cp4_question_2_predict_settlement_amount.csv'
+# df = pd.read_csv (file_path)
+# df = pd.DataFrame(df,columns=['gender', 'race', 'rank', 'interactions', 'outcomes', 'misconducts', 'violences', 'primary_cause', 'total_settlement'])
+# df = df.dropna()
+# df = df[df['total_settlement'] >= 10]
+# y = df[['total_settlement']].astype(int)
+# y = y.transform(lambda x: np.floor(np.log10(x))) # predict the magnitude (e.g. 10 is 1, 100 is 2, 1000 is 3...)
+# X = df[['interactions', 'outcomes', 'misconducts', 'gender', 'race']]
+#
+#
+# # ************* Average settlemt cost by gender *************
+# df2 = df[['gender', 'total_settlement']]
+# df2 = df2[df2['gender'] == 'F']
+# df3 = df[['gender', 'total_settlement']]
+# df3 = df3[df3['gender'] == 'M']
+# plt.bar(['Female','Male'], [np.int(df2.mean()), np.int(df3.mean())], color='red')
+# plt.title("Average settlement cost by gender")
+# plt.show()
+#
+# # ************* Average settlemt cost by race *************
+#
+# df4 = df[['race', 'total_settlement']]
+# df4 = df4[df4['race'] == 'White']
+#
+# df5 = df[['race', 'total_settlement']]
+# df5 = df5[df5['race'] == 'Hispanic']
+#
+# df6 = df[['race', 'total_settlement']]
+# df6 = df6[df6['race'] == 'Black']
+#
+# df7 = df[['race', 'total_settlement']]
+# df7 = df7[df7['race'] == 'Asian/Pacific']
+#
+# df8 = df[['race', 'total_settlement']]
+# df8 = df8[df8['race'] == 'Native American/Alaskan Native']
+#
+# plt.bar(['White','Hispanic', 'Black', 'Asian/Pacific', 'Native American/Alaskan Native'], [np.int(df4.mean()), np.int(df5.mean()), np.int(df6.mean()), np.int(df7.mean()), np.int(df8.mean())], color='red')
+# plt.title("Average settlement cost by race")
+# plt.show()
+#
+#
+#
+# #preprocess data to encode categorical data as numeric
+# le = preprocessing.LabelEncoder()
+# for column_name in X.columns:
+#     if X[column_name].dtype == object:
+#         X[column_name] = le.fit_transform(X[column_name])
+#
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30, random_state = 5)
+# clf = KNeighborsClassifier(n_neighbors=3, weights='distance')
+# clf.fit(X_train, y_train)
+# y_pred = clf.predict(X_test)
+# scores = cross_val_score(clf, X, y, cv=4, scoring='accuracy') #try using 'accuracy', 'precision', 'recall', and 'f1_macro' for the scoring parameter
+# print("CV scores = ",scores)
+# print(metrics.classification_report(y_test, y_pred, digits=3))
