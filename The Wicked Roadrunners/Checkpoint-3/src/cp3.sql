05bb4a0fd6610cf2a46fff0768e42e7946b954e8
Kylie Chesner <kyliec362@gmail.com>
2020-11-30 15:32:11 -0600

    update cp3

--- The Wicked Roadrunners/Checkpoint-3/src/cp3.sql
+++ The Wicked Roadrunners/Checkpoint-3/src/cp3.sql
@@ -1,5 +1,7 @@
-
-select unit_id, total_settlement from
+select t6.unit_id, unit_name, unit_desc, total_settlement from
+(select id as unit_id, unit_name, description as unit_desc from data_policeunit) as t6
+INNER JOIN
+(select unit_id, total_settlement from
 (select officer_id, unit_id, end_date, effective_date from data_officerhistory) AS t3
 inner join
 (select * from
@@ -10,7 +12,11 @@ on t2.lawsuit_id = t1.id) as t4
 on t3.officer_id = t4.officer_id
 where incident_date <= end_date and incident_date >= effective_date
 group by unit_id, total_settlement
-order by unit_id;
+order by unit_id) as t5
+on t5.unit_id = t6.unit_id;
+
+
+
 
 select district_id, incident_date, disciplined from
 (select officer_id, unit_id as district_id, end_date, effective_date from data_officerhistory) AS t4
